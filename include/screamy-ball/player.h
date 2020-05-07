bdb7f1c1c0c6552cec61c1d83cfaad5d3fb75537
ishitarao <ir5@illinois.edu>
2020-05-06 19:50:05 -0500

    Create a Player struct to pass to the leaderboard

---
+++ include/screamy-ball/player.h
@@ -0,0 +1,19 @@
+// Copyright (c) 2020 Ishita Rao. All rights reserved.
+
+#ifndef FINALPROJECT_INCLUDE_SCREAMY_BALL_PLAYER_H_
+#define FINALPROJECT_INCLUDE_SCREAMY_BALL_PLAYER_H_
+
+namespace screamy_ball {
+
+struct Player {
+  Player(const std::string& name, const std::string& elapsed_time) :
+    name(name),
+    elapsed_time(elapsed_time) {}
+
+  std::string name;
+  std::string elapsed_time;
+};
+
+}  // namespace screamy_ball
+
+#endif  // FINALPROJECT_INCLUDE_SCREAMY_BALL_PLAYER_H_
